32f2ba6613a9947a35a22ab2deb1ca88c52ba4ef
Csaba Farago <farago.csaba.phd@gmail.com>
2023-09-06 22:41:39 +0200

    MYMATH-1 Implement prime checker

---
+++ is_prime.py
@@ -0,0 +1,13 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    for i in range(1, 20):
+        if is_prime(i):
+            print(i)
